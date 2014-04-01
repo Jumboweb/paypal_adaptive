6142c3b9254e70e00a44e9baf63bc667f80a562d
Goundar <contact@jumboweb.fr>
2014-04-01 13:39:36 +0200

    Remove #set_form_data from #send_back request

--- lib/paypal_adaptive/ipn_notification.rb
+++ lib/paypal_adaptive/ipn_notification.rb
@@ -15,6 +15,33 @@ module PaypalAdaptive
       @verify_mode = config.verify_mode
     end
 
+    # def send_back(data)
+    #   data = "cmd=_notify-validate&#{data}"
+
+    #   path = "#{@paypal_base_url}/cgi-bin/webscr"
+    #   url = URI.parse path
+    #   http = Net::HTTP.new(url.host, 443)
+    #   http.use_ssl = true
+    #   http.verify_mode = @verify_mode
+      
+    #   http.ca_path = @ssl_cert_path unless @ssl_cert_path.nil?
+
+    #   if @api_cert_file
+    #     cert = File.read(@api_cert_file)
+    #     http.cert = OpenSSL::X509::Certificate.new(cert)
+    #     http.key = OpenSSL::PKey::RSA.new(cert)
+    #   end
+    #   http.ca_path = @ssl_cert_path unless @ssl_cert_path.blank?
+    #   http.ca_file = @ssl_cert_file unless @ssl_cert_file.blank?
+
+
+    #   req = Net::HTTP::Post.new(url.request_uri)
+    #   req.set_form_data(Rack::Utils.parse_nested_query(data))
+    #   req['Accept-Encoding'] = 'identity'
+    #   response_data = http.request(req).body
+    #   @verified = response_data == "VERIFIED"
+    # end
+
     def send_back(data)
       data = "cmd=_notify-validate&#{data}"
       path = "#{@paypal_base_url}/cgi-bin/webscr"
@@ -33,7 +60,12 @@ module PaypalAdaptive
       http.ca_file = @ssl_cert_file unless @ssl_cert_file.blank?
 
       req = Net::HTTP::Post.new(url.request_uri)
-      req.set_form_data(Rack::Utils.parse_nested_query(data))
+      # we don't want #set_form_data to create a hash and get our
+      # response out of order; Paypal IPN docs explicitly state that
+      # the contents of #send_back must be in the same order as they
+      # were recieved
+      req.body = data
+      req.content_type = 'application/x-www-form-urlencoded'
       req['Accept-Encoding'] = 'identity'
       response_data = http.request(req).body
 
